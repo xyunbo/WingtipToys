c94ede84f46ffc10dc6d08b98c517e9b75280c51
xyunbo <xyunbo@yahoo.com>
2015-09-29 13:42:09 -0400

    on chapter Membership and Administration the google account not working

---
+++ WingtipToys/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WingtipToys.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+
+    }
+}
